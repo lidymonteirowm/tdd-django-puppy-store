c2b22665432e8f5a6e187264b5d300b2d07a9345
Lidy Monteiro <lidianedesouzamonteiro@gmail.com>
2018-02-11 01:18:04 -0300

    Database and Model Setup

--- puppy_store/settings.py
+++ puppy_store/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'puppy_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'puppy_store_drf',
+        'USER': 'postgres',
+        'PASSWORD': '010390',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
